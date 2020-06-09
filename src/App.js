093749e3a09f0b763535724cc9e2fd1032577b1b
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-06-09 19:12:48 +0530

    :recycle: setup local env

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classes from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 h1 className = {
+        classes.App
+      }>
+        We are Legion
+      </h1>
     </div>
   );
 }
